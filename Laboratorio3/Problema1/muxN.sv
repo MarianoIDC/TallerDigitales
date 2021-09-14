f4c040718ac3222eb66530cc896593631741b7c7
Mariano Muñoz Masís <mmunoz@estudiantec.cr>
2021-09-14 01:24:33 -0600

    asdfasdf

--- Laboratorio3/Problema1/muxN.sv
+++ Laboratorio3/Problema1/muxN.sv
@@ -6,13 +6,13 @@ module  muxN #(parameter P=4)(OP_1, OP_2, OP_3, OP_4, OP_5, OP_6, OP_7,OP,Result
 	//always @(S)begin
 	always_comb begin
 		case(OP)
-			3'b000:Result<=OP_1;
-			3'b001:Result<=OP_2;
-			3'b010:Result<=OP_3;
-			3'b011:Result<=OP_4;
-			3'b100:Result<=OP_5;
-			3'b101:Result<=OP_6;
-			3'b110:Result<=OP_7;
+			3'b000:Result<=OP_1;//Suma
+			3'b001:Result<=OP_2;//Resta
+			3'b010:Result<=OP_3;//And
+			3'b011:Result<=OP_4;//Or
+			3'b100:Result<=OP_5;//XOR
+			3'b101:Result<=OP_6;//shift left
+			3'b110:Result<=OP_7;//shift right
 			default:Result<=OP_1;
 		endcase
 	end
