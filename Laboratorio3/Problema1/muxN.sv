b826b579b12413d3f2ef4dd2c0cc1f39da4474a3
Mariano Muñoz Masís <mmunoz@estudiantec.cr>
2021-09-14 01:04:13 -0600

    Update muxN.sv

--- Laboratorio3/Problema1/muxN.sv
+++ Laboratorio3/Problema1/muxN.sv
@@ -6,13 +6,13 @@ module  muxN #(parameter P=4)(OP_1, OP_2, OP_3, OP_4, OP_5, OP_6, OP_7,OP,Result
 	//always @(S)begin
 	always_comb begin
 		case(OP)
-			4'b000:Result<=OP_1;
-			4'b001:Result<=OP_2;
-			4'b010:Result<=OP_3;
-			4'b011:Result<=OP_4;
-			4'b100:Result<=OP_5;
-			4'b101:Result<=OP_6;
-			4'b110:Result<=OP_7;
+			3'b000:Result<=OP_1;
+			3'b001:Result<=OP_2;
+			3'b010:Result<=OP_3;
+			3'b011:Result<=OP_4;
+			3'b100:Result<=OP_5;
+			3'b101:Result<=OP_6;
+			3'b110:Result<=OP_7;
 			default:Result<=OP_1;
 		endcase
 	end
