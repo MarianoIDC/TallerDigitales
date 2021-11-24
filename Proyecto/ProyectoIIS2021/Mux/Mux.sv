f1db84bca0f3f68ff119f18c2bfe123e58994562
MarianoIDC <mmunoz@estudiantec.cr>
2021-11-23 21:56:45 -0600

    Mux2to1

--- Proyecto/ProyectoIIS2021/Mux/Mux.sv
+++ Proyecto/ProyectoIIS2021/Mux/Mux.sv
@@ -1,4 +1,4 @@
-module Mux #(parameter N)(input logic [N - 1:0] A, B, input sel, 
+module Mux #(parameter N = 32)(input logic [N - 1:0] A, B, input sel, 
 											output logic [N - 1:0] C);
 assign C = sel ? B : A;
 endmodule
---
+++ Proyecto/ProyectoIIS2021/Mux/Mux2to1_tb.sv
@@ -0,0 +1,19 @@
+module Mux2to1_tb ();
+
+
+parameter N = 32;
+logic [N-1:0] A, B, C;
+logic sel; 
+
+Mux2to1 #(N) iMux (.A(A), .B(B), .C(C), .sel(sel));
+
+initial begin
+    A = 32'b1;
+    B = 32'b0;
+    sel = 1;#10;
+    sel = 0;#10;
+    sel = 1;#10;
+    sel = 0;#10;
+end
+  
+endmodule
