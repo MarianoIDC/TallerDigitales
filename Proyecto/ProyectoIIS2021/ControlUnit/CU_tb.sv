06da7c126f76a228787b8d4f90c456bdfcf8b66d
Luis Martínez Ramírez <louisjo81@gmail.com>
2021-11-24 22:50:02 -0600

    Arreglo tb

--- Proyecto/ProyectoIIS2021/ControlUnit/CU_tb.sv
+++ Proyecto/ProyectoIIS2021/ControlUnit/CU_tb.sv
@@ -5,19 +5,17 @@ module CU_tb ();
 		logic MemToReg, MemToWrite, ALUSrc, PCSrc, RegWrite, MemWrite;
 						  
 		ControlUnit CU(Cond, ALUFlags, Op, Funct, Rd, MemToReg, ALUControl, ALUSrc, ImmSrc, RegSrc, PCSrc, RegWrite, MemWrite);
-				
+		
+		logic [31:0] instruccion = 32'b11100011101000000001000000000000;
+		
 		initial 
 		begin
-		#10
-		Cond = 4'b1110;
-		#10
-		ALUFlags = 4'b000;
-		#10
-		Op = 2'b00;
-		#10
-		Funct = 6'b111010;
-		#10
-		Rd = 4'b0000; 
+		11100100100000010011000000000100;
+		Cond = instruccion [31:28];
+		ALUFlags = 4'b0000;
+		Op = instruccion [27:26];
+		Funct = instruccion [25:20];
+		Rd = instruccion [15:12]; 
 		end
 
 endmodule 
