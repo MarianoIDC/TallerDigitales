d54b02fa5d5de4cf4c7cfc0a3d9184104a85886c
MarianoIDC <mmunoz@estudiantec.cr>
2021-11-24 23:17:33 -0600

    Update CU_tb.sv

--- Proyecto/ProyectoIIS2021/ControlUnit/CU_tb.sv
+++ Proyecto/ProyectoIIS2021/ControlUnit/CU_tb.sv
@@ -1,20 +1,21 @@
 module CU_tb ();
-		logic [3:0] Cond, ALUFlags, Rd;
-		logic [1:0] Op, ImmSrc, RegSrc, ALUControl, FlagW;
-		logic [5:0] Funct;
-		logic MemToReg, MemToWrite, ALUSrc, PCSrc, RegWrite, MemWrite;
 		logic [31:0] instruccion;
-						  
-		ControlUnit CU(instruccion [31:28], 4'b0000, instruccion [27:26], instruccion [25:20], [15:12], MemToReg, ALUControl, ALUSrc, ImmSrc, RegSrc, PCSrc, RegWrite, MemWrite);
+
+		logic [3:0] Cond = instruccion [31:28];
+		logic [3:0] ALUFlags = 4'b0000;
+		logic [3:0] Rd = instruccion [15:12];
+		logic [1:0] Op = instruccion [27:26]; 
+		logic [1:0] ImmSrc, RegSrc, ALUControl, FlagW;
+		logic [5:0] Funct = instruccion [25:20];
+		logic MemToReg, MemToWrite, ALUSrc, PCSrc, RegWrite, MemWrite;
 		
-		
-		
+
+		ControlUnit CU(Cond, ALUFlags, Op, Funct, Rd, MemToReg, ALUControl, ALUSrc, ImmSrc, RegSrc, PCSrc, RegWrite, MemWrite);
+				
 		initial 
 		begin
-		32'b11100100100000010011000000000100;#20;
-		32'b11100011101000000001000000000000;#20;
-		
-	
+			instruccion = 32'b11100100100000010011000000000100;#20
+			instruccion = 32'b11100011101000000001000000000000;#20
 		end
 
 endmodule 
